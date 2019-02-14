8c888a6185431fb4a5caa818fe88a71846161beb
Vicente Silva <vicente.pspenha@gmail.com>
2019-02-13 22:39:11 -0200

    inclusão do serviço e incluído no html botão para listar

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CommentsService } from './comments.service';
 
 @Component({
   selector: 'app-comments',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private commentsService: CommentsService) { }
+  public listComments;
 
   ngOnInit() {
   }
+
+  getListComments(){
+    this.commentsService.getListComments().subscribe((dados) => {
+        this.listComments = dados;
+        console.log(this.listComments[0])
+      },
+      (error) => {
+        console.error('error: ' + error);
+      }
+    )
+  }
+
+  // this.myService.getData(lat, lon)
+  //           .subscribe((result) => {
+  //               this.onGetDataSuccess(result);
+  //           }, (error) => {
+  //               console.log(error);
+  //           });
+  //   }
+  // }
 
 }
--- src/app/comments/comments.service.ts
+++ src/app/comments/comments.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentsService {
 
-  constructor() { }
+  private API_URL = 'https://jsonplaceholder.typicode.com';
+
+  constructor(private httClient: HttpClient) { }
+
+  getListComments(){
+    // let headers = this.createRequestHeader();
+    let httpParams = new HttpParams().set('Authorization', 'auth-token');
+    return this.httClient.get<Comment[]>(this.API_URL + '/posts', {params: httpParams});
+  }
+
+  private createRequestHeader() {
+      // set headers here e.g.
+      let headers = new HttpHeaders({
+          "AuthKey": "my-key",
+          "Authorization": "auth-token",
+          "Content-Type": "application/json",
+          "User-Agent:": "Baeldung Client",
+          // "Time-Zone": "Europe/Amsterdam"
+      });
+
+      return headers;
+  }
 }
